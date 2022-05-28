1fc8a2403559fc847824734dbb5274ee2e2dc0d9
SplatCreator <37952073+SplatCreator@users.noreply.github.com>
2022-05-28 04:17:45 +0200

    Add files via upload

---
+++ indexdaven.php
@@ -0,0 +1,10 @@
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+<title> Hacked by Daven </title> 
+</head><body bgcolor="black"> 
+<table width="100%" height="100%">
+<tbody><tr><td align="center"><br>
+<font face="Courier New" size="4" font="" color="white">Hacked By Daven<br>Contact dav3nsec@protonmail.com for help to fix the website vulnerabilities.</font>
+<audio source="mp3" src="https://l.top4top.io/m_1954kj90i0.mp3" type="mp3" autoplay="autoplay" loop="loop">
+                       </audio>
+</td>
+</tr></tbody></table></body></html>
